4670e037f910bacf5105e43886989d7b70692c5d
Alwin <haklander@gmail.com>
2019-09-10 10:41:03 +0200

    assign different rest-api-ports

--- ubuntu-amd64/start_tmux.sh
+++ ubuntu-amd64/start_tmux.sh
@@ -2,8 +2,12 @@
 
 #the node number can be passed to this script as the 1st argument
 #if no argument is passed, the standard node work folder is used
-if [ "$1" -ge 2 -a "$1" -le 99 ]; then
-	suffix="-$(printf "%02d" $1)"
+node_number=$1
+rest_api_port=8080
+
+if [ "$node_number" -ge 2 -a "$node_number" -le 99 ]; then
+	suffix="-$(printf "%02d" $node_number)"
+	rest_api_port=$((rest_api_port+node_number-1))
 else
 	suffix=""
 fi
@@ -20,4 +24,4 @@ tmux_session_name="testnet$suffix"
 tmux new-session -d -s "$tmux_session_name"
 tmux send -t "$tmux_session_name" "ulimit -n 65535" ENTER
 tmux send -t "$tmux_session_name" "cd $work_folder" ENTER
-tmux send -t "$tmux_session_name" "./node" ENTER
+tmux send -t "$tmux_session_name" "./node --rest-api-port $rest_api_port" ENTER
