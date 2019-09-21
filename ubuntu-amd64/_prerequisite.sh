675c69a66abc0ad7a77327059b8b57d85d75e77e
josefcoap <53934875+josefcoap@users.noreply.github.com>
2019-09-21 12:36:54 +0200

    add distro updates to _prerequisite

--- ubuntu-amd64/_prerequisite.sh
+++ ubuntu-amd64/_prerequisite.sh
@@ -1,5 +1,8 @@
-#install some dependencies
+#Making sure the distro is up-to-date
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
+#install some dependencies
 sudo apt install -y git
 sudo apt install -y curl
 sudo apt install -y screen
