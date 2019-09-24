694096cc2b009d9920b08e0bcc7baf45b76b7c92
Alwin <haklander@gmail.com>
2019-09-24 12:21:45 +0200

    made requested changes to pull request

--- ubuntu-amd64/start_tmux.sh
+++ ubuntu-amd64/start_tmux.sh
@@ -20,15 +20,11 @@ fi
 #after using the install.sh script, we want to run the node from the work folder
 work_folder="$GOPATH/src/github.com/ElrondNetwork/elrond-go-node$suffix"
 
-#check if tmux is already installed and install if necessary
-bash _tmux.sh
-
 #run node in background session: tmux_session_name
 #user can switch to this session by using: tmux a -t $tmux_session_name
 #for a single node, this will be: tmux a -t testnet
 #to detach from that session again: <Ctrl+b>, followed by <d>
 tmux_session_name="testnet$suffix"
 tmux new-session -d -s "$tmux_session_name"
-tmux send -t "$tmux_session_name" "ulimit -n 65535" ENTER
 tmux send -t "$tmux_session_name" "cd $work_folder" ENTER
 tmux send -t "$tmux_session_name" "./node --rest-api-port $rest_api_port" ENTER
