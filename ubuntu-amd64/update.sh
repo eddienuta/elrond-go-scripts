be223da0f9674b37c2dbd1e14536fdda1955be3a
Valentin Lup <valentin.lup@itnt.ro>
2019-09-24 13:43:22 +0300

    Updated node name in update.sh Updated launch options at the end of the update.sh script

--- ubuntu-amd64/update.sh
+++ ubuntu-amd64/update.sh
@@ -9,7 +9,7 @@ CYAN='\x1B[0;36m'
 GREEN='\x1B[0;32m'
 NC='\x1B[0m'
 
-#refetch and rebuild elrond-go
+#Refetch and rebuild elrond-go
 cd $HOME/go/src/github.com/ElrondNetwork/elrond-go
 git fetch
 git checkout --force $BINARYVER
@@ -19,42 +19,66 @@ GO111MODULE=on go mod vendor
 go build -i -v -ldflags="-X main.appVersion=$(git describe --tags --long --dirty)"
 cp node $GOPATH/src/github.com/ElrondNetwork/elrond-go-node
 
-#refetch and rebuild elrond-config
+#Refetch and rebuild elrond-config
 cd $HOME/go/src/github.com/ElrondNetwork/elrond-config
 git fetch
 git checkout --force $CONFIGVER
 git pull
 cp *.* $GOPATH/src/github.com/ElrondNetwork/elrond-go-node/config
 
-#choose node name
-cd $GOPATH/src/github.com/ElrondNetwork/elrond-go-node/config
-read -p "Choose the name of your node (default \"\"): " node_name
-if [ ! "$node_name" = "" ]
-then
-    sed -i 's|NodeDisplayName = ""|NodeDisplayName = "'"$node_name"'"|g' config.toml	
-fi
+#Choose a custom node name... or leave it at default
+echo -e
+echo -e "${GREEN}--> Build ready. Time to choose a node name...${NC}"
+echo -e
 
-#cleanup db
+cd $GOPATH/src/github.com/ElrondNetwork/elrond-go-node/config
+CURRENT=$(sed -e 's#.*-\(\)#\1#' <<< "$CONFIGVER")
+
+read -p "Choose a custom name (default community-validator-$CURRENT): " NODE_NAME
+if [ "$node_name" = "" ]
+then
+    NODE_NAME="community-validator-$CURRENT"
+fi
+sed -i 's/NodeDisplayName = ""/NodeDisplayName = "'$NODE_NAME'"/' $HOME/go/src/github.com/ElrondNetwork/elrond-go-node/config/config.toml
+
+
+#Node DB & Logs Cleanup
 cd $GOPATH/src/github.com/ElrondNetwork/elrond-go-node
-read -p "Remove db? (default yes): " rem_db
+
+read -p "Do you want to remove the current Node DB? (default yes): " rem_db
 if [ "$rem_db" != "no" ]
 then
   sudo rm -rd db
 fi
+
 sudo rm -rd logs
 sudo rm -rd stats
 
 
-#ask about screen launch
-on_screen='no'
-read -p "Launch on virtual screen? (default no): " on_screen
-if [ "$on_screen" = "no" ] | [ "$on_screen" = "n" ] | [ "$on_screen" = "" ]
-then
-  ./node
-else
-  screen -A -m -d -S testnet ./node
-fi
+echo -e
+echo -e "${GREEN}Options for starting your Elrond Node:${NC}"
+echo -e "${CYAN}front${GREEN} - Will start your node in the foreground${NC}"
+echo -e "${CYAN}screen${GREEN} - Will start your node in the backround using the screen app${NC}"
+echo -e "${CYAN}tmux${GREEN} - Will start your node in the backround using the tmux app${NC}"
+echo -e "${CYAN}ENTER${GREEN} - Will exit to the command line without starting your node (in case you need to add previously generated pems)${NC}"
+echo -e
+echo -e
 
-#run the node
+read -p "How do you want to start your node (front|screen|tmux) : " START
 
-
+case $START in
+     front)
+        cd $HOME/elrond-go-scripts/ubuntu-amd64/start_scripts/ && ./start.sh
+        ;;
+     screen)
+        cd $HOME/elrond-go-scripts/ubuntu-amd64/start_scripts/ && ./start_screen.sh
+        ;;
+     
+     tmux)
+        cd $HOME/elrond-go-scripts/ubuntu-amd64/start_scripts/ && ./start_tmux.sh
+        ;;
+     
+     *)
+        echo "Ok ! Have it your way then..."
+        ;;
+esac
