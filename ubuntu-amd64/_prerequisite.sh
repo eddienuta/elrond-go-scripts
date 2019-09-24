6db2319564ade7815282cf30b4cdc209e11d02b0
Valentin Lup <valentin.lup@itnt.ro>
2019-09-24 09:58:31 +0300

    streamlined _prerequisite.sh

--- ubuntu-amd64/_prerequisite.sh
+++ ubuntu-amd64/_prerequisite.sh
@@ -1,8 +1,5 @@
 #Making sure the distro is up-to-date
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt update && sudo apt dist-upgrade -y
 
 #install some dependencies
-sudo apt install -y git
-sudo apt install -y curl
-sudo apt install -y screen
+sudo apt install -y git curl screen
